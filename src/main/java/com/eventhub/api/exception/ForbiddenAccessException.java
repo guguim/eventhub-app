5c4d0e602b1e5a7dc745db9a3e12e20b8e32999e
Hugo Santos <santoshugo527@gmail.com>
2026-09-16 23:16:21 -0300

    feat: add custom exception classes for domain errors

---
+++ src/main/java/com/eventhub/api/exception/ForbiddenAccessException.java
@@ -0,0 +1,8 @@
+package com.eventhub.api.exception;
+
+public class ForbiddenAccessException extends RuntimeException {
+
+    public ForbiddenAccessException(String message) {
+        super(message);
+    }
+}
